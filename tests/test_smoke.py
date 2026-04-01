2b0bff6e664d8f2c758a06c4bc859f9ac157d96e
azbutton <azatsdet@gmail.com>
2026-04-01 14:05:55 -0700

    Trigger build/workflow

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -4,3 +4,4 @@ def test_homepage_smoke_test(smoke_setup):
     assert response.ok
     home_page.expect_page_title_contains_rapidfort()
     home_page.expect_hero_visible()
+# Sample change to trigger a build
